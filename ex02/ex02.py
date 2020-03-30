d7218a1452fdbd1da492f861e3567ba096d5c206
Alan Hahn Pereira <alanhpereira@gmail.com>
2020-03-30 14:41:08 -0300

    remove some unecessary tabs

--- ex02/ex02.py
+++ ex02/ex02.py
@@ -6,7 +6,7 @@ def main() :
 	atual = 0	#número para conferir multilicipidade
 	cont = 0	#contagem de números multiplos de i ou j
 	while cont<n:
-		if atual % i == 0 or atual % j == 0:	
+		if atual % i == 0 or atual % j == 0:
 			print(atual)
 			cont = cont + 1
 		atual = atual + 1
